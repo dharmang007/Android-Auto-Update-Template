524f9f40722baacf009cfd7b8ba097962832f4bf
Dharmang Solanki <dharmang.solanki@sjsu.edu>
2020-06-26 10:42:11 -0700

    Added new api
    -added /latest-version api

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
 from flask import Flask, request
 import json
 from tinydb import TinyDB,Query
-from updatemanager import Application1
-
+from updatemanager import Application1, updatemanager_app, UpdateManager
 
 app = Flask(__name__)
+app.register_blueprint(updatemanager_app)
 db = TinyDB('db.json')
 @app.route('/', methods=['GET'])
 def get_data():
     return json.dumps({"items": db.all()})
 
+@app.route('/latest-version',methods=['GET'])
+def get_latest_version():
+    """
+    Gets latest version for the given app
+    """
+    try:
+        print(request.args.get('appName'))
+        application = Application1()
+        
+        versions_list = application.getVersion(str(request.args.get('appName')))
+        latest = '0.0'
+        for doc in versions_list:
+            if doc['version'] > latest:
+                latest = doc['version']
+        return json.dumps({"latest_version":latest})
+    except Exception as e:
+        print("Error "+ str(e))
+
 @app.route('/versions', methods=['GET'])
-def get_app_versions(appName):
-    print(request.args.get('appName'))
-    application = Application1()
-    appName = request.args.get('appName')
-    versions = application.getVersion(appName)
-    return json.dumps({"items": versions})
+def get_app_versions():
+    """
+    This method gets list of all the version for a specific app
+    For simplicity, I have just used Application1 
+    """
+    try:
+        print(request.args.get('appName'))
+        application = Application1()
+        appName = request.args.get('appName')
+        versions = application.getVersion(appName)
+        return json.dumps({"latest_version": versions})
+    except Exception as e:
+        print(str(e))
+
+    
 
 
 @app.route('/send-update', methods=['GET','POST'])
 def update():
     application = Application1()
-    latest_v = application.send_update("2.0")  
+    latest_v = application.send_update("4.0")  
     return json.dumps({"latest_version":latest_v})
 
 @app.route('/add-version', methods=['GET', 'POST'])
@@ -31,11 +58,7 @@ def add_version():
     add-version endpoint will be used to add a new version of app into the db.json
     """
     print("Adding new version")
-
     return json.dumps(db.all())
-
-
-    
-    
+ 
 if __name__ == '__main__':
     app.run()
--- updatemanager.py
+++ updatemanager.py
@@ -1,11 +1,13 @@
 import abc
 from tinydb import TinyDB,Query
 import json
+from flask import Blueprint
+
+updatemanager_app = Blueprint('updatemanager_app',__name__)
 db = TinyDB('db.json')
 class UpdateManager(abc.ABC):
     """
     Interface for managing updates of multiple apps
-
     """
     def __init__(self):
         pass
@@ -32,37 +34,74 @@ class Application1(UpdateManager):
     """
     Concreate Class which manages updates of Application1
     """
-    def __init__(self,supported_os=[],previous_versions=[],users=[],latest_support_version=""):
+    def __init__(self,supported_os=[],previous_versions=[],users=[],latest_supported_version=""):
         self._supported_os = supported_os
         self._previous_versions = previous_versions
         self._users = users
-        self._latest_support_version = latest_support_version
+        self._latest_supported_version = latest_supported_version
 
     def subscribe(self, user):
         print("User installed this app!")
         # TODO: add into Database
+        # This will be called when the app is installed by the user
         self._users.append(user)
-        return
+        return 
 
     def unsubscribe(self, user):
         print("User uninstalled this app!")
         # TODO: remove from Database
+        # This can be used when the user uninstalls the app 
+        # OR a special code snippet can be added which will manage if the user has disabled the update notification  
         self._users.remove(user) 
         return 
 
+    
     def send_update(self,new_version):
         # TODO: Add the code to add new version for Application1 in DB
         # send notification from server to users to update the app from notifications
-        self._previous_versions.append(self._latest_support_version)
+        self._previous_versions.append(self._latest_supported_version)
         self._latest_supported_version = new_version
         db = TinyDB('db.json')
         db.insert({'app_name':'Application1', 'version':new_version,'location':'','os':'os_1'})
         for user in self._users:
-            user.send_update_notification(new_version)
+            user.get_update_notification(new_version)
+        
         return json.dumps(db.all())
 
-    
+"""
+Users are the different devices which will recieve the updates
+UserInterface is implemented by cconcrete  User class 
+"""
+class UserInterface(abc.ABC):
 
+    @abc.abstractclassmethod
+    def send_update_notification(self,new_version):
+        pass
 
-            
+class Users:
+    def __init__(self,name,email,address):
+        self._name = name
+        self._email = email
+        self._address = address
+
+    def edit_user(self, user):
+        """
+        This method can be used to update user 
+        """
+        pass
+
+    def get_user(self,email):
+        
+        """
+        To retrieve the user details 
+        """
+        pass
+
+    @updatemanager_app.route('/get-notification', methods=['GET', 'POST'])
+    def get_update_notification(self,new_version):
+        
+        """
+        send the API response to send the update to client device
+        """
+        
 
